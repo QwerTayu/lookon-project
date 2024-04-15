f817faf3a7cac4230687bc807feb0eb90ff6766f
QwerTayu <tayu.sa99hire.make@gmail.com>
2024-04-16 00:17:13 +0900

    🍻 環境変数名の変更

--- lookon_project/settings.py
+++ lookon_project/settings.py
@@ -23,7 +23,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
